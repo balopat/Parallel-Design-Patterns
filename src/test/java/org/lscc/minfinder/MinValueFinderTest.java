8078e503abebabb439dd45174e153c88a4ea5b22
Samir Talwar <samir@noodlesandwich.com>
2012-02-25 00:48:41 +0000

    Add generics to MinValueFinderTest and reformat.

--- src/test/java/org/lscc/minfinder/MinValueFinderTest.java
+++ src/test/java/org/lscc/minfinder/MinValueFinderTest.java
@@ -6,24 +6,20 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class MinValueFinderTest {
-
-    private Function square = new Function<Integer,Integer>() {
-        @Override
-        public Integer apply(Integer x) {
+    private Function<Integer, Integer> square = new Function<Integer, Integer>() {
+        @Override public Integer apply(Integer x) {
             return x * x;
         }
     };
 
-    private Function neg = new Function<Integer,Integer>() {
-        @Override
-        public Integer apply(Integer x) {
+    private Function<Integer, Integer> neg = new Function<Integer, Integer>() {
+        @Override public Integer apply(Integer x) {
             return -x;
         }
     };
 
-    private Function slowNeg = new Function<Integer, Integer>() {
-        @Override
-        public Integer apply(Integer x) {
+    private Function<Integer, Integer> slowNeg = new Function<Integer, Integer>() {
+        @Override public Integer apply(Integer x) {
             try {
                 Thread.sleep(200);
             } catch (InterruptedException e) {
@@ -34,39 +30,37 @@ public class MinValueFinderTest {
     };
 
     @Test
-    public void givenSqrFunctionAndIntervalFrom2To5ShouldResultIn4(){
+    public void givenSqrFunctionAndIntervalFrom2To5ShouldResultIn4() {
         MinValueFinder minValueFinderSqr = new MinValueFinder(square);
-        int minValue = minValueFinderSqr.findMinValueBetween(2,5);
+        int minValue = minValueFinderSqr.findMinValueBetween(2, 5);
         assertThat(minValue, is(4));
     }
 
     @Test
-    public void givenSqrFunctionAndIntervalFromMinus4To10ShouldResultIn0(){
+    public void givenSqrFunctionAndIntervalFromMinus4To10ShouldResultIn0() {
         MinValueFinder minValueFinderForSquare = new MinValueFinder(square);
-        int minValue = minValueFinderForSquare.findMinValueBetween(-4,10);
+        int minValue = minValueFinderForSquare.findMinValueBetween(-4, 10);
         assertThat(minValue, is(0));
     }
 
     @Test
-    public void givenNegFunctionAndIntervalFromMinus4To10ShouldResultInMinus10(){
+    public void givenNegFunctionAndIntervalFromMinus4To10ShouldResultInMinus10() {
         MinValueFinder minValueFinderForNeg = new MinValueFinder(neg);
-        int minValue = minValueFinderForNeg.findMinValueBetween(-4,10);
+        int minValue = minValueFinderForNeg.findMinValueBetween(-4, 10);
         assertThat(minValue, is(-10));
     }
 
     @Test
-    public void shouldReturnTheFunctionValueForOneElement(){
+    public void shouldReturnTheFunctionValueForOneElement() {
         MinValueFinder minValueFinderSqr = new MinValueFinder(square);
-        int minValue = minValueFinderSqr.findMinValueBetween(-2,-2);
+        int minValue = minValueFinderSqr.findMinValueBetween(-2, -2);
         assertThat(minValue, is(4));
     }
 
     @Test(timeout = 1000)
-    public void shouldFinishInTime(){
+    public void shouldFinishInTime() {
         MinValueFinder minValueFinderForSlowNeg = new MinValueFinder(slowNeg);
-        int minValue = minValueFinderForSlowNeg.findMinValueBetween(0,10);
+        int minValue = minValueFinderForSlowNeg.findMinValueBetween(0, 10);
         assertThat(minValue, is(-10));
     }
-
-
 }
