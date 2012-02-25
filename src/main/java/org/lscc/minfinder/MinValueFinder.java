896b08b277a14e569db6900a0fa1fab3f73c1067
Samir Talwar <samir@noodlesandwich.com>
2012-02-25 00:49:28 +0000

    Added skeleton MinValueFinder to make the test compile.

---
+++ src/main/java/org/lscc/minfinder/MinValueFinder.java
@@ -0,0 +1,11 @@
+package org.lscc.minfinder;
+
+public class MinValueFinder {
+    public MinValueFinder(Function<Integer, Integer> function) {
+        throw new UnsupportedOperationException();
+    }
+
+    public int findMinValueBetween(int a, int b) {
+        throw new UnsupportedOperationException();
+    }
+}
