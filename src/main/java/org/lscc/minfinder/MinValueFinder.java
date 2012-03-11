ad62b78b11c16cad68aa87716d0a9db4052a02ac
balopat <balopat@balopat.com>
2012-03-11 15:03:23 +0000

    first step, sequential solution only

--- src/main/java/org/lscc/minfinder/MinValueFinder.java
+++ src/main/java/org/lscc/minfinder/MinValueFinder.java
@@ -6,8 +6,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class MinValueFinder extends DivideAndConquer<MinValueFinderProblem, Integer>{
-
+public class MinValueFinder {
 
     private Function<Integer, Integer> f;
 
@@ -16,29 +15,11 @@ public class MinValueFinder extends DivideAndConquer<MinValueFinderProblem, Inte
     }
 
     public int findMinValueBetween(int a, int b) {
-        return solve(new MinValueFinderProblem(a,b,this.f));
-    }
-
-    @Override
-    protected Boolean isBaseCase(MinValueFinderProblem p) {
-        return p.a == p.b;
-    }
-
-    @Override
-    protected Integer baseSolve(MinValueFinderProblem p) {
-        return p.f.apply(p.a);
-    }
-
-    @Override
-    protected Integer merge(List<Integer> subSolutions) {
-        return Collections.min(subSolutions);
-    }
-
-    @Override
-    protected List<MinValueFinderProblem> split(MinValueFinderProblem p) {
-        List<MinValueFinderProblem> result = new ArrayList<MinValueFinderProblem>();
-        result.add(new MinValueFinderProblem(p.a, p.a+(p.b-p.a)/2, p.f));
-        result.add(new MinValueFinderProblem(p.a+(p.b-p.a)/2+1, p.b, p.f));
-        return result;
+        int candidate = f.apply(a);
+        for (int i = a+1; i<=b; i++){
+            int nextValue = f.apply(i);
+            candidate = Math.min(candidate, nextValue);
+        }
+        return candidate;
     }
 }
